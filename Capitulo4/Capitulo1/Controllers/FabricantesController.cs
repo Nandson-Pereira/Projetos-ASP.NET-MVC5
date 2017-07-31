3b616ed81464119eaa2fd660f0e82b8c09f12645
Nandson Silva <nandsonps2@gmail.com>
2017-07-30 23:11:34 -0300

    Adaptando a visao details de fabricantes para utilização de Partial View para mostrar produtos do fabricante

--- Capitulo4/Capitulo1/Controllers/FabricantesController.cs
+++ Capitulo4/Capitulo1/Controllers/FabricantesController.cs
@@ -62,7 +62,7 @@ namespace Capitulo1.Controllers
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest); //erro caso o valor enviado seja nulo
             }
 
-            Fabricante fabricante = context.Fabricantes.Find(id);
+            Fabricante fabricante = context.Fabricantes.Where(f => f.FabricanteId == id).Include("Produtos.Categoria").First();
 
             if (fabricante == null)
             {
