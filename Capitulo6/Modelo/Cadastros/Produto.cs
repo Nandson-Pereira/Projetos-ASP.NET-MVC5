42e45be790938f0290a8a2eb53ccc14f7b28997d
Nandson Silva <nandsonps2@gmail.com>
2017-08-02 21:33:34 -0300

    Inicio Capitulo6

---
+++ Capitulo6/Modelo/Cadastros/Produto.cs
@@ -0,0 +1,17 @@
+﻿using Modelo.Tabelas;
+
+namespace Modelo.Cadastros
+{
+    public class Produto
+    {
+        public int? ProdutoId { get; set; }
+        public string Nome { get; set; }
+
+        public int? CategoriaId { get; set; }
+        public int? FabricanteId { get; set; }
+
+        public Categoria Categoria { get; set; }
+        public Fabricante Fabricante { get; set; }
+
+    }
+}
