76478bb5c3ddb5df5fe2b2b6b100c0985a3fc52d
Nandson Silva <nandsonps2@gmail.com>
2017-08-06 01:09:34 -0300

    implementando o CREATE do usuario, GET e POST da ACTION CREATE

--- Capitulo7/Capitulo1/Areas/Seguranca/Controllers/AdminController.cs
+++ Capitulo7/Capitulo1/Areas/Seguranca/Controllers/AdminController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNet.Identity.Owin;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -13,7 +14,127 @@ namespace Capitulo1.Areas.Seguranca.Controllers
     public class AdminController : Controller
     {
 
-        // GET: CREATE criar a visao para usuario
+
+        // GET: DETAILS criar a visao para usuario  
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Usuario usuario = GerenciadorUsuario.FindById(id);
+
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(usuario);
+        }
+
+
+        // POST: DELETE  action que receberá estes dados, que enfim realizará a persistência deles na base de dados
+        [HttpPost]
+        public ActionResult Delete(Usuario usuario)
+        {
+            Usuario user = GerenciadorUsuario.FindById(usuario.Id);
+
+            if (user != null)
+            {
+                IdentityResult result = GerenciadorUsuario.Delete(user);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return new HttpStatusCodeResult(
+                    HttpStatusCode.BadRequest);
+                }
+            }
+            else
+            {
+                return HttpNotFound();
+            }
+        }
+
+
+        // GET: DEELETE criar a visao para usuario Deletar
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Usuario usuario = GerenciadorUsuario.FindById(id);
+
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(usuario);
+        }
+
+
+        // POST: EDIT  action que receberá estes dados, que enfim realizará a persistência deles na base de dados
+        [HttpPost]
+        public ActionResult Edit(UsuarioViewModel usuarioViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                Usuario usuario = GerenciadorUsuario.FindById(usuarioViewModel.Id);
+
+                usuario.UserName = usuarioViewModel.Nome;
+                usuario.Email = usuarioViewModel.Email;
+                usuario.PasswordHash = GerenciadorUsuario.PasswordHasher.HashPassword(usuarioViewModel.Senha);
+
+                IdentityResult result = GerenciadorUsuario.Update(usuario);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    AddErrorsFromResult(result);
+                }
+            }
+
+            return View(usuarioViewModel);
+        }
+
+
+        // GET: EDIT criar a visao para usuario Editar
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(
+                HttpStatusCode.BadRequest);
+            }
+
+            Usuario usuario = GerenciadorUsuario.FindById(id);
+
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+
+            var usuarioViewModel = new UsuarioViewModel();
+
+            usuarioViewModel.Id = usuario.Id;
+            usuarioViewModel.Nome = usuario.UserName;
+            usuarioViewModel.Email = usuario.Email;
+
+            return View(usuarioViewModel);
+        }
+
+
+        // GET: CREATE criar a visao para usuario criar
         public ActionResult Create()
         {
             return View();
