0441fe8940dd5c357ace4637887e16eed644f17e
Nandson Silva <nandsonps2@gmail.com>
2017-08-05 21:49:06 -0300

    criando a classe para gerenciar usuarios UserManeger

---
+++ Capitulo7/Capitulo1/Infraestrutura/GerenciadorUsuario.cs
@@ -0,0 +1,25 @@
+﻿using Capitulo1.Areas.Seguranca.Models;
+using Capitulo1.DAL;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+
+
+
+namespace Projeto01.Infraestrutura
+{
+    public class GerenciadorUsuario : UserManager<Usuario>
+    {
+        public GerenciadorUsuario(IUserStore<Usuario> store) : base(store)
+        {
+        }
+        public static GerenciadorUsuario Create(IdentityFactoryOptions<GerenciadorUsuario> options, IOwinContext context)
+        {
+            IdentityDbContextAplicacao db = context.Get<IdentityDbContextAplicacao>();
+            GerenciadorUsuario manager = new GerenciadorUsuario(new UserStore<Usuario>(db));
+
+            return manager;
+        }
+    }
+}
