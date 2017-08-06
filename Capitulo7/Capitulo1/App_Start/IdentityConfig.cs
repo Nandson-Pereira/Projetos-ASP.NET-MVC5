ed845d8d52184a79733660bf0c53debd1dc532e8
Nandson Silva <nandsonps2@gmail.com>
2017-08-05 21:56:39 -0300

    implementando  classe inicializadora IdentityConfig na pasta App_Start

---
+++ Capitulo7/Capitulo1/App_Start/IdentityConfig.cs
@@ -0,0 +1,23 @@
+﻿using Capitulo1.DAL;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using Projeto01.Infraestrutura;
+
+namespace Capitulo1
+{
+    public class IdentityConfig
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.CreatePerOwinContext<IdentityDbContextAplicacao>(IdentityDbContextAplicacao.Create);
+            app.CreatePerOwinContext<GerenciadorUsuario>(GerenciadorUsuario.Create);
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Seguranca/Account/Login"),
+            });
+        }
+    }
+}
