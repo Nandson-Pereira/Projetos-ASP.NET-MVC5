db723e9412c3d277fa400f5f4804a4f29115e24b
Nandson Silva <nandsonps2@gmail.com>
2017-08-09 23:24:27 -0300

    inserindo novos papais

--- Capitulo7/Capitulo1/Areas/Seguranca/Controllers/PapelAdminController.cs
+++ Capitulo7/Capitulo1/Areas/Seguranca/Controllers/PapelAdminController.cs
@@ -1,7 +1,10 @@
-﻿using Capitulo1.Infraestrutura;
+﻿using Capitulo1.Areas.Seguranca.Models;
+using Capitulo1.Infraestrutura;
+using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -14,6 +17,54 @@ namespace Capitulo1.Areas.Seguranca.Controllers
         public ActionResult Index()
         {
             return View(RoleManager.Roles);
+        }
+
+
+        //GET: Create Papeis/Roles
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        //POST: Create Papeis/Roles
+        [HttpPost]
+        public ActionResult Create([Required]string nome)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityResult result = RoleManager.Create(new Papel(nome));
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    AddErrorsFromResult(result);
+                }
+            }
+
+            return View(nome);
+        }
+
+
+
+        //metodos de obter configuração e metodos auxiliares as actions
+        private void AddErrorsFromResult(IdentityResult result)
+        {
+            foreach (string error in result.Errors)
+            {
+                ModelState.AddModelError("", error);
+            }
+        }
+
+        private GerenciadorUsuario UserManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().GetUserManager<GerenciadorUsuario>();
+            }
         }
 
         private GerenciadorPapel RoleManager
